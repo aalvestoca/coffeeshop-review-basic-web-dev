1ae9f603c7ea64ee80400b3e8ee7e877036b6121
aalvestoca <aalvestoca@gmail.com>
2024-07-17 11:59:10 -0400

    test git user

--- menu.js
+++ menu.js
@@ -3,8 +3,8 @@ var productNameArray = [];
 
 function getProductNames(callback) {
     fetch("./starbucks-nutrition.json").then(text => text.json()).then(products => {
-        productArray = products
-        productNameArray = Array.from(new Set(products.map(product => product.Product_Name)))
+        productArray = products;
+        productNameArray = Array.from(new Set(products.map(product => product.Product_Name)));
             
         callback();
     })
