9bf037795cbcb7775fed00d922acd0e3e28a884b
alexalvesdev <alex3elloni@gmail.com>
2024-07-17 11:46:44 -0400

    menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
-var productArray = []
-var productNameArray = []
+var productArray = [];
+var productNameArray = [];
 
 function getProductNames(callback) {
     fetch("./starbucks-nutrition.json").then(text => text.json()).then(products => {
         productArray = products
         productNameArray = Array.from(new Set(products.map(product => product.Product_Name)))
             
-        callback()
+        callback();
     })
 }
